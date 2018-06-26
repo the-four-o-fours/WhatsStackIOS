00dc02abefed79a83b982fa63a16f35df05eaa42
Chloe Chong <chloechong.us@gmail.com>
2018-06-26 14:49:20 -0400

    Added component to save user to database

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { View, Button, Text, TextInput, Image } from 'react-native';
 
 import firebase from 'react-native-firebase';
 
+import CreateUser from './src/components/createUser';
+
 const successImageUri =
   'https://cdn.pixabay.com/photo/2015/06/09/16/12/icon-803718_1280.png';
 
@@ -139,23 +141,24 @@ export default class PhoneAuthTest extends Component {
         {!user && confirmResult && this.renderVerificationCodeInput()}
 
         {user && (
-          <View
-            style={{
-              padding: 15,
-              justifyContent: 'center',
-              alignItems: 'center',
-              backgroundColor: '#77dd77',
-              flex: 1,
-            }}
-          >
-            <Image
-              source={{ uri: successImageUri }}
-              style={{ width: 100, height: 100, marginBottom: 25 }}
-            />
-            <Text style={{ fontSize: 25 }}>Signed In!</Text>
-            <Text>{JSON.stringify(user)}</Text>
-            <Button title="Sign Out" color="red" onPress={this.signOut} />
-          </View>
+          <CreateUser />
+          // <View
+          //   style={{
+          //     padding: 15,
+          //     justifyContent: 'center',
+          //     alignItems: 'center',
+          //     backgroundColor: '#77dd77',
+          //     flex: 1,
+          //   }}
+          // >
+          //   <Image
+          //     source={{ uri: successImageUri }}
+          //     style={{ width: 100, height: 100, marginBottom: 25 }}
+          //   />
+          //   <Text style={{ fontSize: 25 }}>Signed In!</Text>
+          //   <Text>{JSON.stringify(user)}</Text>
+          //   <Button title="Sign Out" color="red" onPress={this.signOut} />
+          // </View>
         )}
       </View>
     );
