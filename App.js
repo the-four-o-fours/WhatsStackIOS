319229e3fda1f1009218baaaafcf32b23e2b620f
Chloe Chong <chloechong.us@gmail.com>
2018-06-26 23:30:48 -0400

    Added loading circle

--- App.js
+++ App.js
@@ -1,40 +1,41 @@
-import { createStackNavigator } from 'react-navigation';
+import {createStackNavigator} from 'react-navigation'
 
-import Main from './src/components/Main';
-import Login from './src/components/auth/Login';
-import CreateUser from './src/components/auth/CreateUser';
-import AllChats from './src/components/chat/AllChats';
+import Main from './src/components/Main'
+import Login from './src/components/auth/Login'
+import CreateUser from './src/components/auth/CreateUser'
+import AllChats from './src/components/chat/AllChats'
 
 const RootNavigator = createStackNavigator(
   {
     Main: {
       screen: Main,
-      navigationOptions: ({ navigation }) => ({
+      navigationOptions: ({navigation}) => ({
         title: 'WhatsStack',
       }),
     },
     Login: {
       screen: Login,
-      navigationOptions: ({ navigation }) => ({
+      navigationOptions: ({navigation}) => ({
         title: 'Login',
       }),
     },
     CreateUser: {
       screen: CreateUser,
-      navigationOptions: ({ navigation }) => ({
+      navigationOptions: ({navigation}) => ({
         title: 'Set display name',
       }),
     },
     Chat: {
       screen: AllChats,
-      navigationOptions: ({ navigation }) => ({
+      navigationOptions: ({navigation}) => ({
         title: 'Chat',
+        headerLeft: null,
       }),
     },
   },
   {
     initialRouteName: 'Main',
-  }
-);
+  },
+)
 
-export default RootNavigator;
+export default RootNavigator
--- src/components/chat/AllChats.js
+++ src/components/chat/AllChats.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import {View, Button, Text, TextInput, Image} from 'react-native';
+import React from 'react'
+import {View, Button, Text, TextInput, Image} from 'react-native'
 
-import firebase from 'react-native-firebase';
+import firebase from 'react-native-firebase'
 
 const AllChats = () => {
+  const signOut = () => {
+    firebase.auth().signOut()
+  }
   return (
     <View>
+      <Button title="Sign Out" color="red" onPress={signOut} />
       <Text>AllChats Component</Text>
     </View>
-  );
-};
+  )
+}
 
-export default AllChats;
+export default AllChats
