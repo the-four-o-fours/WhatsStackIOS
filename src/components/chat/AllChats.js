83407dab2ba019593ccc6e039c7bf544fdb0bcf6
siameseguy <psyhakhom@gmail.com>
2018-06-28 17:05:15 -0400

    feature(chat):designing chat

--- src/components/chat/AllChats.js
+++ src/components/chat/AllChats.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import {View, Button, Text, TextInput, Image} from 'react-native';
+import {View, TextInput, StyleSheet} from 'react-native';
 import Chat from './Chat'
 
 import firebase from 'react-native-firebase';
 
 const AllChats = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Chat/>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 5,
+    backgroundColor: "#fff"
+  }
+});
 
 export default AllChats;
--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react'
-import firebase from 'react-native-firebase';
-import {StyleSheet, Text, AsyncStorage, View} from 'react-native';
-import {Container, Button, Form, Item, Input} from 'native-base';
+import firebase from 'react-native-firebase'
+import {StyleSheet, Text, View, FlatList, TextInput} from 'react-native'
+import {Content, Button, Form, Item, Input} from 'native-base'
+import {Col, Row, Grid} from "react-native-easy-grid";
 const moment = require('moment')
+
+import ChatItem from './ChatItem'
 
 class Chat extends Component {
   constructor() {
     super()
 
     this.state = {
-      messages: '',
+      message: '',
+      messages: [],
       uid: ''
     }
   }
@@ -17,47 +21,119 @@ class Chat extends Component {
   componentDidMount() {
     const {uid, phoneNumber} = firebase
       .auth()
-      .currentUser;
-    this.setState({uid});
-
+      .currentUser
+    this.setState({uid})
+    this.listener()
   }
 
   sendMessage = text => {
-    console.log("sendmessage reached")
-    const myMessage = {
+    const senderMessage = {
       text,
       sender: true,
       group: false
     }
-    const recipientMessage = {
+    const receiverMessage = {
       text,
       sender: false,
       group: false
     }
-
     const sentAt = moment(Date.now())
-    const nousitRef = firebase
+    const chloeRef = firebase
       .database()
-      .ref('Users/oRag3daBrTeXDXv70LUngnnM9eV2/3q2Pf5aMjeWuEi2TK8sNX6LrB3F3') //Nousit's ID / my ID //un-hard-code eventually
+      .ref('Users/ME8NBZ125PbgVrJVCWVma2mCbnF2/P0xLKKiHwNfP1asdgX8blSq8pMa2') //chloe's ID / my ID //un-hard-code eventually
     const spencerRef = firebase
       .database()
-      .ref('Users/3q2Pf5aMjeWuEi2TK8sNX6LrB3F3/oRag3daBrTeXDXv70LUngnnM9eV2') //my ID / Nousit's ID //un-hard-code eventually
+      .ref('Users/P0xLKKiHwNfP1asdgX8blSq8pMa2/ME8NBZ125PbgVrJVCWVma2mCbnF2') //my ID / chloe's ID //un-hard-code eventually
     const senderMessageObj = {}
     const receiverMessageObj = {}
-    senderMessageObj[sentAt] = myMessage
-    receiverMessageObj[sentAt] = recipientMessage
-    nousitRef.update(senderMessageObj)
+    senderMessageObj[sentAt] = senderMessage
+    receiverMessageObj[sentAt] = receiverMessage
+    chloeRef.update(senderMessageObj)
     spencerRef.update(receiverMessageObj)
   }
 
-  render() {
+  listener = () => {
+    const conversationRef = firebase
+      .database()
+      .ref('Users/ME8NBZ125PbgVrJVCWVma2mCbnF2/P0xLKKiHwNfP1asdgX8blSq8pMa2') //chloe-me convo
+    conversationRef.on('value', snapshot => {
+      const messages = []
+      snapshot.forEach(message => {
+        const messageObj = {}
+        messageObj[message.key] = message.val()
+        messages.push(messageObj)
+      })
+      this.setState({messages})
+    })
+  }
 
+  onChangeHandler = (message) => {
+    this.setState({message})
+  }
+
+  onSubmitHandler = () => {
+    console.log('hello!')
+  }
+
+  render() {
+    console.log('uid', this.state.uid)
     return (
-      <View>
-        <Text>{this.sendMessage('Hello thing!')}</Text>
+      <View style={styles.container}>
+        <Grid>
+          <Row size={90}>
+            <View>
+              <ChatItem messages={this.state.messages}/>
+            </View>
+          </Row>
+          <Row size={10}>
+            <Grid>
+              <Row>
+                <Col size={85}>
+                  <Content>
+                    <Item>
+                      <Input placeholder="Enter Message" onChange={this.onChangeHandler}/>
+                    </Item>
+                  </Content>
+                </Col>
+                <Col size={15}>
+                  <Button
+                    block
+                    success
+                    style={styles.submitBtn}
+                    onPress={this.placeSubmnitHandler}>
+                    <Text style={{
+                      color: '#fff'
+                    }}>Send</Text>
+                  </Button>
+                </Col>
+              </Row>
+            </Grid>
+          </Row>
+        </Grid>
+
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between"
+  },
+  inputContainer: {
+    flex: 1,
+    // width: "100%", flexDirection: "row", justifyContent: "space-between",
+    // alignItems: "center"
+  },
+  textInput: {
+    // width: "85%"
+    margin: 5
+  },
+  submitBtn: {
+    width: "100%",
+    color: "#fff"
+  }
+});
 
 export default Chat
