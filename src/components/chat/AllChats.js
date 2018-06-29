e26b1fcfe6214332c7ee826d4e0742c867c65c85
spencer <spencerwhitehead7@gmail.com>
2018-06-29 18:02:57 -0400

    remove: dead code

--- src/components/chat/AllChats.js
+++ src/components/chat/AllChats.js
@@ -2,37 +2,12 @@ import React, {Component} from 'react'
 import {View, Button} from 'react-native'
 import {connect} from 'react-redux'
 
-import Chat from './Chat'
-
 import firebase from 'react-native-firebase'
 
 import {getNewMessage} from '../../store/actions'
 
 class AllChats extends Component {
-  componentDidMount() {
-    const uid = this.props.user.uid
-    // const userRef = firebase.database().ref(`/Users/${uid}`)
-    // userRef.off()
-    // userRef.on('child_added', snapshot => {
-    //   console.log('key', snapshot.key)
-    //   console.log('child added', snapshot.val())
-    // })
-    // userRef.on('child_changed', snapshot => {
-    //   console.log('Child changed', snapshot.val())
-    // })
-
-    // Get chatIds from user object in redux then listen to each one
-    // userRef.once('value', snapshot => {
-    //   const user = snapshot.val()
-    //   for (let key in user) {
-    //     if (typeof user[key] === 'object') {
-    //       const ref = firebase.database().ref(`/Users/${uid}/${key}`)
-    //       ref.off()
-    //       ref.on('child_added', this.getNewMessageCallBack())
-    //     }
-    //   }
-    // })
-  }
+  componentDidMount() {}
 
   getNewMessageCallBack = (snapshot, prevChildKey) => {
     const messageObj = {...snapshot.val()}
@@ -41,17 +16,24 @@ class AllChats extends Component {
     console.log('prevChild', prevChildKey)
   }
 
+  componentWillUnmount() {}
+
   signOut = () => {
     firebase.auth().signOut()
   }
 
-  componentWillUnmount() {}
+  goToConvo = uid => {
+    this.props.navigation.navigate('Chat', {
+      uid,
+    })
+  }
 
   render() {
     return (
       <View>
         <Button title="Sign Out" color="red" onPress={this.signOut} />
-        {/* <Chat /> */}
+        <Button title="Sign Out" color="red" onPress={this.goToConvo} />
+        <Button title="Sign Out" color="red" onPress={this.goToConvo} />
       </View>
     )
   }
