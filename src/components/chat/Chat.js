b4506759f226db8b123578450d2dcdf81ba88540
spencer <spencerwhitehead7@gmail.com>
2018-06-29 17:10:21 -0400

    removes moment

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react'
 import firebase from 'react-native-firebase'
 import {StyleSheet, Text, AsyncStorage, View} from 'react-native'
 import {Container, Button, Form, Item, Input} from 'native-base'
-const moment = require('moment')
 
 class Chat extends Component {
   constructor() {
@@ -31,7 +30,7 @@ class Chat extends Component {
       sender: false,
       group: false,
     }
-    const sentAt = moment(Date.now())
+    const sentAt = Date.now()
     const chloeRef = firebase
       .database()
       .ref('Users/ME8NBZ125PbgVrJVCWVma2mCbnF2/P0xLKKiHwNfP1asdgX8blSq8pMa2') //chloe's ID / my ID //un-hard-code eventually
