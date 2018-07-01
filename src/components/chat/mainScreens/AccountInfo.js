af2f978ca06d562abb1c2e8cf954ec3b0fb1ad6c
Chloe Chong <chloechong.us@gmail.com>
2018-07-01 17:56:49 -0400

    Feat: conditional rendering to change displayname

--- src/components/chat/mainScreens/AccountInfo.js
+++ src/components/chat/mainScreens/AccountInfo.js
@@ -14,6 +14,7 @@ import firebase from 'react-native-firebase'
 
 class AccountInfo extends React.Component {
   state = {
+    change: false,
     displayName: '',
   }
   signOut = () => {
@@ -25,7 +26,11 @@ class AccountInfo extends React.Component {
       .database()
       .ref(`/Users/${this.props.user.uid}/displayName`)
     userRef.set(this.state.displayName)
-    this.setState({displayName: ''})
+    this.setState({displayName: '', change: false})
+  }
+
+  changeView = () => {
+    this.setState({change: true})
   }
 
   render() {
@@ -34,18 +39,27 @@ class AccountInfo extends React.Component {
         <ScrollView>
           <View style={styles.container}>
             <Text>AccountInfo Dummy Component</Text>
-            <Text>Change your displayname:</Text>
-            <TextInput
-              value={this.state.displayName}
-              onChangeText={displayName => this.setState({displayName})}
-              onSubmitEditing={this.changeDisplayName}
-            />
-            <TouchableOpacity
-              onPress={this.changeDisplayName}
-              disabled={!this.state.displayName.length}
-            >
-              <Text>Enter</Text>
-            </TouchableOpacity>
+            {this.state.change ? (
+              <View>
+                <Text>Change your displayname:</Text>
+                <TextInput
+                  value={this.state.displayName}
+                  onChangeText={displayName => this.setState({displayName})}
+                  onSubmitEditing={this.changeDisplayName}
+                />
+                <TouchableOpacity
+                  onPress={this.changeDisplayName}
+                  disabled={!this.state.displayName.length}
+                >
+                  <Text>Enter</Text>
+                </TouchableOpacity>
+              </View>
+            ) : (
+              <View>
+                <Text>{this.props.user.displayName}</Text>
+                <Button title="Change" color="red" onPress={this.changeView} />
+              </View>
+            )}
             <Button title="Sign Out" color="red" onPress={this.signOut} />
           </View>
         </ScrollView>
