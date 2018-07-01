428f8dd02b97ec6ebbdb9cca949bcfd4b860fbf3
spencer <spencerwhitehead7@gmail.com>
2018-06-30 22:55:32 -0400

    feat: dummy account info component

---
+++ src/components/chat/userscreens/AccountInfo.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {View, StyleSheet, Text} from 'react-native'
+
+class Contacts extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Dummy Component</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
+
+export default Contacts
