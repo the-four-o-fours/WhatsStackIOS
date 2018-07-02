2f75ba1ddcbdd9a12a556c3c1200fca4d4f9e489
spencer <spencerwhitehead7@gmail.com>
2018-07-01 23:03:28 -0400

    feat: search contacts, avoid keyboard

--- src/components/chat/mainScreens/Contacts.js
+++ src/components/chat/mainScreens/Contacts.js
@@ -1,41 +1,66 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {View, StyleSheet} from 'react-native'
+import {View, StyleSheet, TextInput, KeyboardAvoidingView} from 'react-native'
 import {ListItem} from 'react-native-elements'
 
 class Contacts extends React.Component {
-  goToConvoOrNewMessage = (uid, title) => {
-    if (this.props.messages[uid]) {
-      this.props.navigation.navigate('Chat', {
-        uid,
-        title,
-      })
-    } else {
-      this.props.navigation.navigate('NewChat')
-      //we should add something so it starts you on a draft of a message to that user
-      //once we actually finish the newchat component and understand how it will work
+  constructor(props) {
+    super(props)
+    this.state = {
+      matchingContacts: [],
     }
+  }
+
+  componentDidMount() {
+    this.setState({
+      matchingContacts: this.props.contactsArr,
+    })
+  }
+
+  goToConvo = (uid, title) => {
+    this.props.navigation.navigate('Chat', {
+      uid,
+      title,
+    })
+  }
+
+  searchFor = contactName => {
+    const matchingContacts = this.props.contactsArr.filter(contact =>
+      contact.displayName.startsWith(contactName),
+    )
+    this.setState({
+      matchingContacts,
+    })
   }
 
   render() {
     return (
-      <View style={styles.container}>
+      <KeyboardAvoidingView
+        enabled
+        behavior="padding"
+        keyboardVerticalOffset={64}
+        styles={styles.container}
+      >
+        <TextInput
+          autoFocus={true}
+          placeholder="Contact Name"
+          value={this.state.searchFor}
+          onChangeText={contactName => this.searchFor(contactName)}
+        />
         {this.props.contactsArr.length ? (
           <View>
-            {this.props.contactsArr.map(contact => (
+            {this.state.matchingContacts.map(contact => (
               <ListItem
                 key={contact.uid}
                 title={`${contact.phoneName} (${contact.displayName})`}
-                onPress={() =>
-                  this.goToConvoOrNewMessage(contact.uid, contact.displayName)
-                }
+                onPress={() => this.goToConvo(contact.uid, contact.displayName)}
               />
             ))}
           </View>
         ) : (
           <Text>No Contacts</Text>
         )}
-      </View>
+      </KeyboardAvoidingView>
     )
   }
 }
