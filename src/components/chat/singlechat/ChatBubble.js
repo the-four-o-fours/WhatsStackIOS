661a395a0d36b6b4fa7150b78c39b06a959d02da
spencer <spencerwhitehead7@gmail.com>
2018-07-04 22:25:43 -0400

    refactor: uses new sender value on message object

--- src/components/chat/singlechat/ChatBubble.js
+++ src/components/chat/singlechat/ChatBubble.js
@@ -2,7 +2,8 @@ import React from 'react'
 import {StyleSheet, View, Text} from 'react-native'
 
 const ChatBubble = props => {
-  const {message} = props
+  const {message, user} = props
+
   const isToday = () => {
     const now = new Date(Date.now()).toString()
     const messageDate = new Date(Number(message.timeStamp)).toString()
@@ -13,20 +14,20 @@ const ChatBubble = props => {
     }
   }
 
-  if (message.sender) {
+  if (message.sender === user.uid) {
     return (
       <View style={[styles.container, styles.senderBubble]}>
         <View style={[styles.bubble, styles.senderInnerBubble]}>
           <Text style={styles.messageText}>{props.message.text}</Text>
           <Text style={styles.timeStampText}>{`\n${isToday()}`}</Text>
         </View>
-        <View style={[styles.triangle, styles.senderTriangle]}/>
+        <View style={[styles.triangle, styles.senderTriangle]} />
       </View>
     )
   } else {
     return (
       <View style={[styles.container, styles.receiverBubble]}>
-        <View style={[styles.triangle, styles.receiverTriangle]}/>
+        <View style={[styles.triangle, styles.receiverTriangle]} />
         <View style={[styles.bubble, styles.receiverInnerBubble]}>
           <Text style={styles.text}>{props.message.text}</Text>
           <Text style={styles.timeStampText}>{`\n${isToday()}`}</Text>
@@ -38,36 +39,36 @@ const ChatBubble = props => {
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row'
+    flexDirection: 'row',
   },
   bubble: {
     maxWidth: 335,
     padding: 10,
     marginBottom: 5,
     marginTop: 5,
-    borderRadius: 10
+    borderRadius: 10,
   },
   senderBubble: {
-    alignSelf: 'flex-end'
+    alignSelf: 'flex-end',
   },
   receiverBubble: {
-    alignSelf: 'flex-start'
+    alignSelf: 'flex-start',
   },
   senderInnerBubble: {
     backgroundColor: '#006994',
-    borderTopRightRadius: 0
+    borderTopRightRadius: 0,
   },
   receiverInnerBubble: {
     backgroundColor: '#AEE8C3',
-    borderBottomLeftRadius: 0
+    borderBottomLeftRadius: 0,
   },
   messageText: {
     fontSize: 16,
-    color: 'white'
+    color: 'white',
   },
   timeStampText: {
     fontSize: 12,
-    color: '#E8FDFF'
+    color: '#E8FDFF',
   },
   triangle: {
     width: 0,
@@ -77,10 +78,10 @@ const styles = StyleSheet.create({
     borderRightWidth: 12,
     borderTopWidth: 12,
     borderRightColor: 'transparent',
-    marginTop: 5
+    marginTop: 5,
   },
   senderTriangle: {
-    borderTopColor: '#006994'
+    borderTopColor: '#006994',
   },
   receiverTriangle: {
     borderTopColor: '#AEE8C3',
@@ -88,10 +89,10 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     transform: [
       {
-        rotate: '180deg'
-      }
-    ]
-  }
+        rotate: '180deg',
+      },
+    ],
+  },
 })
 
 export default ChatBubble
