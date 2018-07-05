48257fcb454faf5590058a3dfb647755b5f1e12e
spencer <spencerwhitehead7@gmail.com>
2018-07-05 14:11:16 -0400

    feat: handles listening for new group messages

--- src/store/actions/messages.js
+++ src/store/actions/messages.js
@@ -1,10 +1,16 @@
 export const GOT_MESSAGES = 'GOT_MESSAGES'
+export const GOT_GROUP_MESSAGES = 'GOT_GROUP_MESSAGES'
 export const GOT_NEW_MESSAGE = 'GOT_NEW_MESSAGE'
 export const SAW_MESSAGE = 'SAW_MESSAGE'
 
 export const getMessages = messages => ({
   type: GOT_MESSAGES,
   messages,
+})
+
+export const getGroupMessages = groupMessages => ({
+  type: GOT_GROUP_MESSAGES,
+  groupMessages,
 })
 
 export const getNewMessage = (conversation, chatId) => ({
--- src/store/reducers/messages.js
+++ src/store/reducers/messages.js
@@ -1,4 +1,9 @@
-import {GOT_MESSAGES, GOT_NEW_MESSAGE, SAW_MESSAGE} from '../actions'
+import {
+  GOT_MESSAGES,
+  GOT_GROUP_MESSAGES,
+  GOT_NEW_MESSAGE,
+  SAW_MESSAGE,
+} from '../actions'
 
 const initialState = {}
 
@@ -8,6 +13,11 @@ const messagesReducer = (state = initialState, action) => {
       const chatId = Object.keys(action.messages)[0]
       if (!state[chatId]) action.messages[chatId].seen = false
       return {...state, ...action.messages}
+    }
+    case GOT_GROUP_MESSAGES: {
+      const chatId = Object.keys(action.groupMessages)[0]
+      if (!state[chatId]) action.groupMessages[chatId].seen = false
+      return {...state, ...action.groupMessages}
     }
     case GOT_NEW_MESSAGE:
       return {
