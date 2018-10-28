975a04c548f94fc9ce4be4f304b6f7c0c7e2bd5d
spencer <spencerwhitehead7@gmail.com>
2018-10-28 02:41:47 -0400

    refactor: generalizes and modularizes bubbles

--- src/components/chat/chatScreen/ChatBubble.js
+++ src/components/chat/chatScreen/ChatBubble.js
@@ -16,57 +16,48 @@ const ChatBubble = props => {
     })
   }
 
-  if (message.sender === user.uid) {
+  const bubbleCore = (specStyles, showName) => (
+    <View style={[genStyles.bubble, specStyles.innerBubble]}>
+      {showName && (
+        <Text style={specStyles.timeStampText}>{`${displayName}\n`}</Text>
+      )}
+      {message.img ? (
+        <TouchableOpacity onPress={goToImage}>
+          <Image
+            resizeMode="contain"
+            style={genStyles.image}
+            source={{uri: message.text}}
+          />
+        </TouchableOpacity>
+      ) : (
+        <Text style={specStyles.messageText}>{message.text}</Text>
+      )}
+      <Text style={specStyles.timeStampText}>{`\n${formattedDate}`}</Text>
+    </View>
+  )
+
+  const sentByYou = message.sender === user.uid
+  let specStyles = senStyles
+  let showName = false
+  if (!sentByYou) {
+    specStyles = recStyles
+    showName = isGChat
     return (
-      <View style={[styles.container, styles.senderBubble]}>
-        <View style={[styles.bubble, styles.senderInnerBubble]}>
-          {message.img ? (
-            <TouchableOpacity onPress={goToImage}>
-              <Image
-                resizeMode="contain"
-                style={styles.image}
-                source={{uri: message.text}}
-              />
-            </TouchableOpacity>
-          ) : (
-            <Text style={styles.senderMessageText}>{message.text}</Text>
-          )}
-          <Text style={styles.senderTimeStampText}>{`\n${formattedDate}`}</Text>
-        </View>
-        <View style={[styles.triangle, styles.senderTriangle]} />
-      </View>
-    )
-  } else {
-    return (
-      <View style={[styles.container, styles.receiverBubble]}>
-        <View style={[styles.triangle, styles.receiverTriangle]} />
-        <View style={[styles.bubble, styles.receiverInnerBubble]}>
-          {isGChat && (
-            <Text style={styles.receiverTimeStampText}>
-              {`${displayName}\n`}
-            </Text>
-          )}
-          {message.img ? (
-            <TouchableOpacity onPress={goToImage}>
-              <Image
-                resizeMode="contain"
-                style={styles.image}
-                source={{uri: message.text}}
-              />
-            </TouchableOpacity>
-          ) : (
-            <Text style={styles.receiverMessageText}>{message.text}</Text>
-          )}
-          <Text
-            style={styles.receiverTimeStampText}
-          >{`\n${formattedDate}`}</Text>
-        </View>
+      <View style={[genStyles.container, specStyles.bubble]}>
+        <View style={[genStyles.triangle, specStyles.triangle]} />
+        {bubbleCore(specStyles, showName)}
       </View>
     )
   }
+  return (
+    <View style={[genStyles.container, specStyles.bubble]}>
+      {bubbleCore(specStyles, showName)}
+      <View style={[genStyles.triangle, specStyles.triangle]} />
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({
+const genStyles = StyleSheet.create({
   container: {
     flexDirection: 'row',
   },
@@ -76,36 +67,6 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     marginTop: 5,
     borderRadius: 10,
-  },
-  senderBubble: {
-    alignSelf: 'flex-end',
-  },
-  receiverBubble: {
-    alignSelf: 'flex-start',
-  },
-  senderInnerBubble: {
-    backgroundColor: '#006994',
-    borderTopRightRadius: 0,
-  },
-  receiverInnerBubble: {
-    backgroundColor: '#AEE8C3',
-    borderBottomLeftRadius: 0,
-  },
-  senderMessageText: {
-    fontSize: 16,
-    color: 'white',
-  },
-  senderTimeStampText: {
-    fontSize: 12,
-    color: 'white',
-  },
-  receiverMessageText: {
-    fontSize: 16,
-    color: '#4a4c4f',
-  },
-  receiverTimeStampText: {
-    fontSize: 12,
-    color: '#4a4c4f',
   },
   triangle: {
     width: 0,
@@ -117,10 +78,50 @@ const styles = StyleSheet.create({
     borderRightColor: 'transparent',
     marginTop: 5,
   },
-  senderTriangle: {
+  image: {
+    width: 300,
+    height: 220,
+  },
+})
+
+const senStyles = StyleSheet.create({
+  bubble: {
+    alignSelf: 'flex-end',
+  },
+  innerBubble: {
+    backgroundColor: '#006994',
+    borderTopRightRadius: 0,
+  },
+  messageText: {
+    fontSize: 16,
+    color: 'white',
+  },
+  timeStampText: {
+    fontSize: 12,
+    color: 'white',
+  },
+  triangle: {
     borderTopColor: '#006994',
   },
-  receiverTriangle: {
+})
+
+const recStyles = StyleSheet.create({
+  bubble: {
+    alignSelf: 'flex-start',
+  },
+  innerBubble: {
+    backgroundColor: '#AEE8C3',
+    borderBottomLeftRadius: 0,
+  },
+  messageText: {
+    fontSize: 16,
+    color: '#4a4c4f',
+  },
+  timeStampText: {
+    fontSize: 12,
+    color: '#4a4c4f',
+  },
+  triangle: {
     borderTopColor: '#AEE8C3',
     alignSelf: 'flex-end',
     marginBottom: 5,
@@ -129,10 +130,6 @@ const styles = StyleSheet.create({
         rotate: '180deg',
       },
     ],
-  },
-  image: {
-    width: 300,
-    height: 220,
   },
 })
 
